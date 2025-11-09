2f0fd5faa19a4e131ca9b6a04eb18648bc7c2d56
Evan Bollig <ebbollig@amazon.com>
2025-11-09 10:26:30 -0600

    plink depends on cxx and c

--- repos/spack_repo/builtin/packages/plink/package.py
+++ repos/spack_repo/builtin/packages/plink/package.py
@@ -43,6 +43,7 @@ class Plink(Package):
         depends_on("lapack", when="@1.9-beta6.27:")
     depends_on("gmake", type="build")
     depends_on("c", type="build")
+    depends_on("cxx", type="build")
 
     patch("dynamic_zlib.patch", when="@1.9-beta6.27:1.9-beta6.99")
     patch("dynamic_zlib-1.3.patch", when="@1.9-beta7.7:")
