682349c5b7d81c228afcd8d5dff985eb00e4b5ec
Evan Bollig <ebbollig@amazon.com>
2025-11-24 15:44:48 -0600

    Fix ctffind

--- repos/spack_repo/builtin/packages/ctffind/package.py
+++ repos/spack_repo/builtin/packages/ctffind/package.py
@@ -26,6 +26,7 @@ class Ctffind(AutotoolsPackage):
         extension="tar.gz",
     )
 
+    depends_on("c", type="build")  # generated
     depends_on("cxx", type="build")  # generated
 
     def url_for_version(self, version):
